d5e85cb503a7ca18a036840055d40f3701d37d44
LRomero <ldnavar5@gmail.com>
2020-04-02 16:47:56 -0500

    created a browse.js file to save all of our browsing code. Added code that will save the information from the api to our db by user. Added model for what users are watching.

---
+++ models/watching.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+    var Watching = sequelize.define("Watching", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+    });
+  
+    Watching.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      Watching.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Watching;
+  };
--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,3 @@
-
-
-function displayMovieInfo() {
-
-  var movie = $("#movie-input").val();
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-
-    var movieDiv = $("<div class='movie'>");
-    var rating = response.Rated;
-    var pOne = $("<p>").text("Rating: " + rating);
-    movieDiv.append(pOne);
-    var released = response.Released;
-    var pTwo = $("<p>").text("Released: " + released);
-    movieDiv.append(pTwo);
-    var plot = response.Plot;
-    var pThree = $("<p>").text("Plot: " + plot);
-    movieDiv.append(pFour);
-    var type = response.Type;
-    var pFour = $("<p>").text("Type: " + type);
-    movieDiv.append(pFour);
-    var imgURL = response.Poster;
-    var image = $("<img>").attr("src", imgURL);
-    movieDiv.append(image);
-    $("#movies-view").prepend(movieDiv);
-    console.log(response);
-  });
-
-}
-
-$("#add-movie").on("click", displayMovieInfo);
-
 $(document).ready(function() {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
