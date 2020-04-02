19d7e6371c1cc5b89f5e6ad1edace89d737a7bbe
Leighton <leighton.shallenberger@gmail.com>
2020-04-02 18:11:41 -0500

    cleaned up search display code

--- public/js/browse.js
+++ public/js/browse.js
@@ -9,23 +9,20 @@ function displayMovieInfo() {
     }).then(function (response) {
   
       var movieDiv = $("<div class='movie'>");
+      var addBtn = $("<button id ='testing'>");
+      addBtn.append("Add to Library");
       var rating = response.Rated;
       var pOne = $("<p>").text("Rating: " + rating);
       movieDiv.append(pOne);
       var released = response.Released;
       var pTwo = $("<p>").text("Released: " + released);
-      movieDiv.append(pTwo);
-      var plot = response.Plot;
-      var pThree = $("<p>").text("Plot: " + plot);
-      movieDiv.append(pFour);
-      var type = response.Type;
-      var pFour = $("<p>").text("Type: " + type);
-      movieDiv.append(pFour);
+      movieDiv.append(pTwo);;
       var imgURL = response.Poster;
       var image = $("<img>").attr("src", imgURL);
       movieDiv.append(image);
-      $("#movies-view").prepend(movieDiv);
-      console.log(response);
+      $("#movies-view").empty();
+      $("#movies-view").append(movieDiv);
+      $("#movies-view").append(addBtn);
      
       var savebtn = $("<button>").text('SAVE').addClass("savedbtn");
       savebtn.attr('name', response.Title);
