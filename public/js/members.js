2951eb69a4e36563a64e03141e9c61940572a4b0
Leighton <leighton.shallenberger@gmail.com>
2020-04-02 20:18:57 -0500

    added loop to members.js to display library

--- public/js/members.js
+++ public/js/members.js
@@ -5,3 +5,33 @@ $(document).ready(function() {
     $(".member-name").text(data.email);
   });
 });
+
+function getWatching() {
+  $.get("/api/watching", function(data) {
+    console.log(data);
+    data.forEach(function(element) {
+      var movie = element.title;
+      var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+    
+        var movieDiv = $("<div class='movie'>");
+        var rating = response.Rated;
+        var pOne = $("<p>").text("Rating: " + rating);
+        movieDiv.append(pOne);
+        var released = response.Released;
+        var pTwo = $("<p>").text("Released: " + released);
+        movieDiv.append(pTwo);;
+        var imgURL = response.Poster;
+        var image = $("<img>").attr("src", imgURL);
+        movieDiv.append(image);
+        $("#library").append(movieDiv);
+      });
+    });
+    
+  });
+};
+
+getWatching();
